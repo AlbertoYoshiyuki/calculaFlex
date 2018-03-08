e589903d66f6b826d0571e0d8056e9210b3e30b8
ayoshiyuki <ayoshiyuki@travelace.com.br>
2018-03-08 13:13:33 -0300

    funcao do botao limpar incluido

--- app/src/main/java/com/yoshiyuki/alberto/calculaflex/MainActivity.kt
+++ app/src/main/java/com/yoshiyuki/alberto/calculaflex/MainActivity.kt
@@ -2,11 +2,19 @@ package com.yoshiyuki.alberto.calculaflex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLimpar.setOnClickListener{
+            inputAlcool.editText?.setText("")
+            inputGasolina.editText?.setText("")
+            inputGasolina.editText?.requestFocus()
+        }
+
     }
 }
